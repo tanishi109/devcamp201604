f4bd3128583f4dfec3b45753a53658a395370166
MasatoHayakawa <MasatoHayakawa@tanishi-no-MacBook-Pro.local>
2016-04-10 00:40:33 +0900

    上まで行ったら下からスクロールし直す

--- dive/GameScene.swift
+++ dive/GameScene.swift
@@ -18,48 +18,135 @@ class GameScene: SKScene {
     private var touching = false
     // 少しずつ移動させる
     private var lastScrollDistY: CGFloat = 0.0
-    
+    // 動かせるやつ
+    private var saintZone = TouchableNode()
     
     override func didMoveToView(view: SKView) {
 
         // なんか置いてみる
-        let touchableNode = TouchableNode()
-        touchableNode.position = CGPoint(
-            x:CGRectGetMidX(self.frame),
-            y:CGRectGetMidY(self.frame)
+        self.saintZone.position = CGPoint(
+            x: CGRectGetMidX(self.frame),
+            y: CGRectGetMidY(self.frame)
         )
-        self.addChild(touchableNode)
-    }
-    
-    override func update(currentTime: CFTimeInterval) {
 
+        self.addChild(self.saintZone)
     }
 
+    // タッチ開始
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        self.touching = true
-        // store the starting position of the touch
-        let touch = touches.first
-        let location = touch!.locationInNode(self)
-        startY = location.y
-        lastY = location.y
-    }
-    
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        // Declare the touched symbol and its location on the screen
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
             let touchNode = self.nodeAtPoint(location)
             if (touchNode.name == "touchable") {
+                self.touching = true
+                let touch = touches.first
+                let location = touch!.locationInNode(self)
+                startY = location.y
+                lastY = location.y
+            }
+        }
+
+    }
+    
+    // タッチ移動中
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // Declare the touched symbol and its location on the screen
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            if (self.saintZone.containsPoint(location)) {
                 let touch = touches.first
                 let location = touch!.locationInNode(self)
                 let currentY = location.y
                 lastScrollDistY =  lastY - currentY
-                touchNode.position.y -= lastScrollDistY
-                // Set new last location for next time
+                self.saintZone.position.y -= lastScrollDistY
+//                self.saintZone.position.y = currentY // 試し
+                
                 lastY = currentY
 
             }
         }
+    }
+
+    // タッチ終わり
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.touching = false
+    }
+    
+    // タッチキャンセル
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        self.touching = false
+    }
+
+    // 毎秒処理
+    override func update(currentTime: CFTimeInterval) {
+        // タッチされてなかったら
+        if !touching {
+            // 上と下端の設定
+//            let limitFactor: CGFloat = 0.3
+//            let topLimitY: CGFloat = 0 * (-limitFactor)
+//            let bottomLimitY: CGFloat = self.size.height * limitFactor
+//            if self.saintZone.position.y < topLimitY {
+//                // 行き過ぎたから戻す
+//                self.saintZone.position.y = topLimitY
+//                lastScrollDistY = 0.0
+//                return
+//            }
+//            if self.saintZone.position.y > bottomLimitY {
+//                // 行き過ぎたから戻す
+//                self.saintZone.position.y = bottomLimitY
+//                lastScrollDistY = 0.0
+//                return
+//            }
+            
+            // 慣性処理
+            var slowDown: CGFloat = 0.98
+            if fabs(lastScrollDistY) < 0.5 {
+                slowDown = 0.0
+            }
+            lastScrollDistY *= slowDown
+            self.saintZone.position.y -= lastScrollDistY
+        }
+
+        // 高さがある程度行ったら
+        let topSaintZone    = self.saintZone.children[0]
+        let topSaintZonePos = self.convertPoint(topSaintZone.position, fromNode: self.saintZone)
+        
+        if topSaintZonePos.y > self.size.height {
+
+            NSLog("*****")
+            self.saintZone.removeAllChildren()
+            self.saintZone = TouchableNode()
+            self.saintZone.position = CGPoint(
+                x:CGRectGetMidX(self.frame),
+                y:CGRectGetMidY(self.frame)
+            )
+            self.addChild(self.saintZone)
+
+            
+//            self.saintZone.removeChildrenInArray([topSaintZone])
+//            NSLog("\(self.saintZone.children.count)")
+//
+//            let block = SKSpriteNode(
+//                color: UIColor.greenColor(),
+//                size: CGSizeMake(UIScreen.mainScreen().bounds.size.width, 300)
+//            )
+//
+//            block.alpha = 0.25
+//            block.name = "touchable"
+//
+//            block.position = CGPoint(
+//                x: 0,
+//                y: 0
+//            )
+//
+//            self.saintZone.addChild(block)
+//
+//            NSLog("\(self.saintZone.children.count)")
+//            NSLog("\(self.saintZone.children)")
+//            NSLog("***\n\n\n\n")
+
+        }
+
     }
 }
 
--- dive/TouchableNode.swift
+++ dive/TouchableNode.swift
@@ -4,32 +4,37 @@ class TouchableNode: SKNode {
 
     override init() {
         super.init()
-        
-        self.addFireNode()
-        NSLog("*** init")
+        self.addSaintZones(self.getRandom)
     }
     
-    private func addFireNode() {
+    private func addSaintZones(getRand: () -> CGFloat) {
+
         // なんか置いてみる
         let block = SKSpriteNode(
             color: UIColor.greenColor(),
-            size: CGSizeMake(100, 100)
+            size: CGSizeMake(UIScreen.mainScreen().bounds.size.width, getRand())
         )
-        
+
+
         block.alpha = 0.25
         block.name = "touchable"
-        
-        block.position = CGPoint(
-            x:CGRectGetMidX(self.frame)-10,
-            y:CGRectGetMidY(self.frame)
-        )
-        
-        self.addChild(block)
 
+        block.position = CGPoint(
+            x: 0,
+            y: CGRectGetMidY(self.frame)
+        )
+
+        self.addChild(block)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    private func getRandom() -> CGFloat {
+       NSLog("called")
+       return CGFloat(Int((arc4random_uniform(10)+1)*100))
     }
 
 }
